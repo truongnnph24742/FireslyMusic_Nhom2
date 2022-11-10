7318033fd8e29894a07f5de6496f3cf77f0973be
TruongNguyen <truongnnph24742@fpt.edu.vn>
2022-11-10 09:51:16 +0700

    Trường khởi tạo dự án FireslyMusic

---
+++ app/src/main/java/com/example/fireslymusic_nhom2_cp17310/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fireslymusic_nhom2_cp17310;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
