9dcfe5565604bf95a2af278f8f80b660fb46c60d
kiên <kienduong7903@gmail.com>
2022-11-10 21:09:59 +0700

    kiên làm màn hình đăng kí

--- app/src/main/java/com/example/fireslymusic_nhom2_cp17310/Activity/ManHinhDangKiActivity.java
+++ app/src/main/java/com/example/fireslymusic_nhom2_cp17310/Activity/ManHinhDangKiActivity.java
@@ -2,19 +2,15 @@ package com.example.fireslymusic_nhom2_cp17310.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.example.fireslymusic_nhom2_cp17310.R;
 
-public class ManHinhDangNhapActivity extends AppCompatActivity {
+public class ManHinhDangKiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_man_hinh_dang_nhap);
-
-
+        setContentView(R.layout.activity_man_hinh_dang_ki);
     }
 }
