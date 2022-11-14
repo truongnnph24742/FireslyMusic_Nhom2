e968f838d455efdf8c2355c155e786d57a03be5d
HungNguyen <hungntph24736@fpt.edu.vn>
2022-11-14 18:04:56 +0700

    Nguyễn Thế Hưng làm khung giao diện fragment

--- app/src/main/java/com/example/fireslymusic_nhom2_cp17310/MainActivity.java
+++ app/src/main/java/com/example/fireslymusic_nhom2_cp17310/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.fireslymusic_nhom2_cp17310;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.fireslymusic_nhom2_cp17310.Fragment.HomeFragment;
+import com.example.fireslymusic_nhom2_cp17310.Fragment.LiberyFragment;
+import com.example.fireslymusic_nhom2_cp17310.Fragment.ProfileFragment;
+import com.example.fireslymusic_nhom2_cp17310.Fragment.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        replaceFragment(new HomeFragment());
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+           switch (item.getItemId()){
+               case R.id.home :
+                   replaceFragment(new HomeFragment());
+                   break;
+               case R.id.search:
+                   replaceFragment(new SearchFragment());
+                   break;
+               case R.id.libery:
+                   replaceFragment(new LiberyFragment());
+                   break;
+               case R.id.pers:
+                   replaceFragment(new ProfileFragment());
+                   break;
+           }
+           return true;
+        });
+
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framechinh,fragment);
+        fragmentTransaction.commit();
     }
 }
